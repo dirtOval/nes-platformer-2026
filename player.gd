f34e44e9c622a4b4a7b76c5f7113659d9d38b93a
Deirdre <deirdre.loyall@gmail.com>
2026-05-30 17:48:03 -0400

    added coyote time

--- player.gd
+++ player.gd
@@ -4,19 +4,34 @@ extends CharacterBody2D
 @export var SPEED = 200.0
 @export var JUMP_VELOCITY = -400.0
 
+var on_floor_ref = true
+
+#physics process frame are fixed at 60/second
+@export var coyote_frames: int = 6
+var coyote_timer: int = 0
+
 
 func _physics_process(delta: float) -> void:
   # Add the gravity.
   if not is_on_floor():
     velocity += get_gravity() * delta
-
+    
   # Handle jump.
-  if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-    velocity.y = JUMP_VELOCITY
-
+  if Input.is_action_just_pressed("jump"):
+    if is_on_floor() or coyote_timer > 0:
+      velocity.y = JUMP_VELOCITY
+  
+  if coyote_timer > 0:
+    coyote_timer -= 1
+    
+  if is_on_floor() != on_floor_ref:
+    if on_floor_ref == true:
+      coyote_timer = coyote_frames
+  on_floor_ref = is_on_floor()
+  
   # Get the input direction and handle the movement/deceleration.
   # As good practice, you should replace UI actions with custom gameplay actions.
-  var direction := Input.get_axis("ui_left", "ui_right")
+  var direction := Input.get_axis("left", "right")
   if direction:
     velocity.x = direction * SPEED
   else:
