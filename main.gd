78e500aa6e484f9bb2d30f6dabcc3b2ed4a8d9aa
Deirdre <deirdre.loyall@gmail.com>
2026-05-30 20:22:38 -0400

    added reset button, acceleration for movement (though it sucks rn)

---
+++ main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const player = preload("res://player.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+  pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+  if Input.is_action_just_pressed("reset"):
+    print("reset scene")
+    $Camera2D.queue_free()
+    get_tree().reload_current_scene() 
--- player.gd
+++ player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 200.0
-@export var JUMP_VELOCITY = -400.0
+@export var MAX_SPEED = 200.0
+@export var JUMP_VELOCITY = -400
+@export var ACCELERATION = 1000
+@export var FRICTION = 1200
+
 
 var on_floor_ref = true
 
@@ -10,8 +13,17 @@ var on_floor_ref = true
 @export var coyote_frames: int = 6
 var coyote_timer: int = 0
 
-
 func _physics_process(delta: float) -> void:
+   
+  # Get the input direction and handle the movement/deceleration.
+  # As good practice, you should replace UI actions with custom gameplay actions.
+  var direction := Input.get_axis("left", "right")
+  if direction:
+    #velocity.x = direction * MAX_SPEED
+    velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
+  else:
+    velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+    
   # Add the gravity.
   if not is_on_floor():
     velocity += get_gravity() * delta
@@ -28,14 +40,6 @@ func _physics_process(delta: float) -> void:
     if on_floor_ref == true:
       coyote_timer = coyote_frames
   on_floor_ref = is_on_floor()
-  
-  # Get the input direction and handle the movement/deceleration.
-  # As good practice, you should replace UI actions with custom gameplay actions.
-  var direction := Input.get_axis("left", "right")
-  if direction:
-    velocity.x = direction * SPEED
-  else:
-    velocity.x = move_toward(velocity.x, 0, SPEED)
 
   move_and_slide()
 
@@ -43,7 +47,7 @@ func die() -> void:
   var camera = $Camera2D
   var camera_position = camera.global_position
   remove_child($Camera2D)
-  get_tree().root.add_child(camera)
+  get_tree().root.get_node("Main").add_child(camera)
   camera.position_smoothing_enabled = false
   camera.global_position = camera_position
   queue_free()
